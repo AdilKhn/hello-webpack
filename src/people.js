e30f07754149badeabd8e2811a0e6fe83532e718
Adil Khan <adil.khan@outlook.com>
2017-07-23 10:29:09 -0500

    tweaks

---
+++ src/people.js
@@ -0,0 +1,21 @@
+const people = [
+  {
+    manager: 'Jen',
+    name: 'Bob'
+  },
+  {
+    manager: 'Jen',
+    name: 'Sue'
+  },
+  {
+    manager: 'Bob',
+    name: 'Shirely'
+  },
+  {
+    manager: 'Bob',
+    name: 'Terrance'
+  },
+
+];
+
+export default people;
